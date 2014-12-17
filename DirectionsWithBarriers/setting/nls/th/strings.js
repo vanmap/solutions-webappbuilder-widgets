0f22b488a6e7d3ba2aadba9e1448ab890d78f3ae
Kevin Gonzago <kgonzago@esri.com>
2014-12-17 08:34:53 -0500

    Updated settings directory

--- DirectionsWithBarriers/setting/nls/th/strings.js
+++ DirectionsWithBarriers/setting/nls/th/strings.js
@@ -1,22 +1,7 @@
 ﻿define({
-
-    routeUrl:"ก้_Route URL_ษฺ",
-    locatorUrl:"ก้_Locator URL_ษฺ",
-    geocoderOptions:"ก้_Geocoder options_ษฺ",
-    autoComplete:"ก้_Auto complete_ษฺ",
-    maxLocations:"ก้_Max locations_ษฺ",
-    minCharacters:"ก้_Min characters_ษฺ",
-    searchDelay:"ก้_Search delay_ษฺ",
-    placeholder:"ก้_Placeholder_ษฺ",
-    routeOptions:"ก้_Route options_ษฺ",
-    directionsLanguage:"ก้_Directions language_ษฺ",
-    directionsLengthUnits:"ก้_Directions length units_ษฺ",
-    directionsOutputType:"ก้_Directions output type_ษฺ",
-    impedanceAttribute:"ก้_Impedance attribute_ษฺ",
-    kilometers: 'ก้_Kilometers_ษฺ',
-    miles: 'ก้_Miles_ษฺ',
-    meters: 'ก้_Meters_ษฺ',
-    feet: 'ก้_Feet_ษฺ',
-    yards: 'ก้_Yards_ษฺ',
-    nauticalMiles:'ก้_Nautical Miles_ษฺ'
+    left: "ก้_LEFT_ษฺ",
+    right: "ก้_RIGHT_ษฺ",
+    arrangement: "การจัดเรียง",
+    autoUpdate: "อัพเดทอัตโนมัติ",
+    respectCurrentMapScale: "เชื่อในมาตราส่วนแผนที่ปัจจุบัน"
 });

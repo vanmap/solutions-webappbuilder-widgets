dda0b3398bb0ba44d3880c970fd14d881d49736b
Michael Miller <mmiller@esri.com>
2015-01-21 21:54:56 -0500

    GeoEnrich redesign

---
+++ GeoLookup/setting/nls/strings.js
@@ -0,0 +1,26 @@
+define({
+    root: {
+        settingsHeader: "Blah Blah",
+        settingsDesc: "Blah Blah",
+        layerTable: {
+            colEnrich: "Enrich",
+            colLabel: "Layer",
+            colFieldSelector: "Fields"
+        },
+        fieldTable: {
+            colAppend: "Append",
+            colName: "Name",
+            colAlias: "Alias"
+        },
+        symbolArea:{
+            symbolLabelWithin: ' Select the symbol for locations within:',
+            symbolLabelOutside: 'Select the symbol for locations outside:'
+        },
+        noPolygonLayers: "No Polygon Layers",
+        errorOnOk: "Please fill out all parameters before saving config",
+        next: "Next",
+        back: "Back",
+        saveFields: "Save Fields",
+        cancelFields: "Cancel Fields"
+    }
+});

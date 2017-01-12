3cafc6f5a2465100cdc716fc331e24d3d911c2b6
Matt Funk <mfunk@esri.com>
2017-01-11 16:20:41 -0800

    Updated Viewshed tool to accept elevation service as second input

--- Visibility/gpservices/Viewshed/Viewshed.py
+++ Visibility/gpservices/Viewshed/Viewshed.py
@@ -5,6 +5,8 @@ import arcpy, math
 ########Draw Wedge Function##################
 ####
 
+DEBUG = True
+
 def drawWedge(cx,cy,r1,r2,start,end):
     point = arcpy.Point()
     array = arcpy.Array()
@@ -70,21 +72,24 @@ def drawWedge(cx,cy,r1,r2,start,end):
 ########Script Parameters##################
 ####
 Point_Input = arcpy.GetParameterAsText(0)
-Radius2_Input = arcpy.GetParameterAsText(1)
-Azimuth1_Input = arcpy.GetParameterAsText(2)
-Azimuth2_Input = arcpy.GetParameterAsText(3)
-OffsetA_Input = arcpy.GetParameterAsText(4)
-Radius1_Input = arcpy.GetParameterAsText(5)
-viewshed = arcpy.GetParameterAsText(6)
-wedge = arcpy.GetParameterAsText(7)
-fullwedge = arcpy.GetParameterAsText(8)
-elevation = arcpy.GetParameterAsText(9)
+elevation = arcpy.GetParameterAsText(1)
+Radius2_Input = arcpy.GetParameterAsText(2)
+Azimuth1_Input = arcpy.GetParameterAsText(3)
+Azimuth2_Input = arcpy.GetParameterAsText(4)
+OffsetA_Input = arcpy.GetParameterAsText(5)
+Radius1_Input = arcpy.GetParameterAsText(6)
+viewshed = arcpy.GetParameterAsText(7)
+wedge = arcpy.GetParameterAsText(8)
+fullwedge = arcpy.GetParameterAsText(9)
 
+# Call image service to get SR
+arcpy.MakeImageServerLayer_management(elevation, "elevation")
 
-Output_CS = arcpy.Describe(elevation).spatialReference
+elevDesc = arcpy.Describe("elevation")
+Output_CS = elevDesc.spatialReference
 if not Output_CS.type == "Projected":
     arcpy.AddError("Data Error: Input elevation raster must be in a projected coordinate system. Existing elevation raster is in {0}.".format(Output_CS.name))
-    
+
 arcpy.env.outputCoordinateSystem = Output_CS
 
 polylist = []
@@ -103,7 +108,7 @@ arcpy.CalculateField_management(Point_Input, "AZIMUTH1", Azimuth1_Input, "PYTHON
 arcpy.CalculateField_management(Point_Input, "AZIMUTH2", Azimuth2_Input, "PYTHON_9.3", "")
 arcpy.CalculateField_management(Point_Input, "OFFSETA", OffsetA_Input, "PYTHON_9.3", "")
 arcpy.CalculateField_management(Point_Input, "RADIUS1", Radius1_Input, "PYTHON_9.3", "")
-
+arcpy.AddMessage("Buffering observers...")
 arcpy.Buffer_analysis(Point_Input, "in_memory\OuterBuffer", "RADIUS2", "FULL", "ROUND", "NONE", "", "GEODESIC")
 
 desc = arcpy.Describe("in_memory\OuterBuffer")
@@ -112,10 +117,15 @@ yMin = desc.Extent.YMin
 xMax = desc.Extent.XMax
 yMax = desc.Extent.YMax
 Extent = str(xMin) + " " + str(yMin) + " " + str(xMax) + " " + str(yMax)
-arcpy.Clip_management(elevation, Extent, "in_memory\clip")
-
+# Call image service a second time to get corrected extents
+arcpy.env.extent = Extent
+arcpy.env.mask = "in_memory\\OutBuffer"
+arcpy.AddMessage("Clipping image to observer buffer...")
+arcpy.MakeImageServerLayer_management(elevation, "elevation", Extent, "#", "#", "#", "#", "#", elevDesc.meanCellWidth)
+arcpy.Clip_management("elevation", Extent, "in_memory\clip")
+arcpy.AddMessage("Calculating viewshed...")
 arcpy.Viewshed_3d("in_memory\clip", Point_Input, "in_memory\intervis", "1", "FLAT_EARTH", "0.13")
-
+arcpy.AddMessage("Creating features from raster...")
 arcpy.RasterToPolygon_conversion(in_raster="in_memory\intervis", out_polygon_features="in_memory\unclipped",simplify="NO_SIMPLIFY")
 
 fields = ["SHAPE@XY","RADIUS1","RADIUS2","AZIMUTH1","AZIMUTH2"]
@@ -141,6 +151,6 @@ with arcpy.da.SearchCursor(Point_Input,fields) as cursor:
 
 arcpy.CopyFeatures_management(polylist,wedge)
 arcpy.CopyFeatures_management(wedges,fullwedge)
-
+arcpy.AddMessage("Finishing output features...")
 arcpy.Clip_analysis("in_memory\unclipped", wedge, "in_memory\\dissolve")
 arcpy.Dissolve_management("in_memory\\dissolve", viewshed, "gridcode", "", "MULTI_PART", "DISSOLVE_LINES")

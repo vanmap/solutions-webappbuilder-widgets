df6ca7266568f119f19abf2c019f90cea54cd153
Chris Moore <csmooreacm@acm.org>
2017-04-12 15:55:26 -0400

    Fix issue with checking if observers within surface
    1. Mostly a workaround - units and extents were not always being checked
    correctly (seemed like arcpy bug)
    2. Also added parameter list to viewshed method so it could be unit
    tested

--- Visibility/gpservices/Viewshed/Viewshed.py
+++ Visibility/gpservices/Viewshed/Viewshed.py
@@ -29,21 +29,7 @@ import arcpy
 import math
 import os
 
-####
-########Script Parameters##################
-####
-inputPoints = arcpy.GetParameterAsText(0)
-elevationRaster = arcpy.GetParameterAsText(1)
-Radius2_Input = arcpy.GetParameterAsText(2)
-Azimuth1_Input = arcpy.GetParameterAsText(3)
-Azimuth2_Input = arcpy.GetParameterAsText(4)
-OffsetA_Input = arcpy.GetParameterAsText(5)
-Radius1_Input = arcpy.GetParameterAsText(6)
-viewshed = arcpy.GetParameterAsText(7)
-sectorWedge = arcpy.GetParameterAsText(8)
-fullWedge = arcpy.GetParameterAsText(9)
-
-DEBUG = False
+DEBUG = True
 
 def drawWedge(cx,cy,r1,r2,start,end):
     point = arcpy.Point()
@@ -103,62 +89,95 @@ def drawWedge(cx,cy,r1,r2,start,end):
 
     return polygon
 
-def surfaceContainsPoint(pointFeature, surfRaster):
+def surfaceContainsPoint(pointFeatures, surfRaster):
     '''
-    Check if point extent falls within surface extent, return True or False
+    Check if points fall within surface extent, return True or False
+    Assumes pointFeatures have been projected to the same SR as surfRaster prior to this call
     '''
     surfDesc = arcpy.Describe(surfRaster)
-    surfaceExtentPoly = extentToPoly(surfDesc.extent, surfDesc.spatialReference)
+    pointsDesc = arcpy.Describe(pointFeatures)
+    
+    surfaceSR = surfDesc.spatialReference
+    pointsSR = pointsDesc.spatialReference
 
-    pointDesc = arcpy.Describe(pointFeature)
-    pointExtent = pointDesc.extent
-    pointFeature = extentToPoly(pointExtent, pointDesc.spatialReference)
-    #project poly to same sr as surface
-    pointFeatProj = pointFeature.projectAs(surfDesc.spatialReference)
+    if surfaceSR.Name != pointsSR.Name : 
+        raise Exception('surfaceContainsPoint: Spatial References do not match: ' \
+            + pointsSR.Name + ' != ' + surfaceSR.Name)
 
-    isWithin = pointFeatProj.within(surfaceExtentPoly)
+    surfaceExtent = surfDesc.extent
 
-    if DEBUG: arcpy.AddMessage("Within: {0}".format(isWithin))
+    pointRows = arcpy.da.SearchCursor(pointFeatures, ["SHAPE@"])
+
+    for pointRow in pointRows:
+    
+        point = pointRow[0]   
+        x = point.firstPoint.X  
+        y = point.firstPoint.Y 
+
+        # WORKAROUND: 
+        # these were not returning reliable results, so computing manually:
+        # pointProj = point.projectAs(surfaceSR)   
+        # isWithin = surfaceExtent.contains(pointProj) # pointProj.within(surfaceExtent)  
+        isWithin = (x >= surfaceExtent.XMin) and (x <= surfaceExtent.XMax) and \
+            (y >= surfaceExtent.YMin) and (y <= surfaceExtent.YMax)
+          
+        if not isWithin : 
+            arcpy.AddMessage("Point:({0}, {1})\n Within:({2})\n sr: {3}\n".format(x, y, \
+                surfaceExtent, surfaceSR.name))
+            break
+
+    if DEBUG: arcpy.AddMessage("Input Points Within Surface: {0}".format(isWithin))
+
     return isWithin
 
-def extentToPoly(extent, sr):
-    '''
-    Extent object returns arcpy.Polygon or arcpy.Point
-    '''
-    if extent.height == 0.0 and extent.width == 0.0:
-        if DEBUG: arcpy.AddMessage("point: {0}\n sr: {1}".format(extent, sr.name))
-        return arcpy.PointGeometry(extent.lowerLeft, sr)
-    else:
-        if DEBUG: arcpy.AddMessage("poly: {0}\n sr: {1}".format(extent, sr.name))
-        return extent.polygon
+# Solution reused from:
+# http://joshwerts.com/blog/2015/09/10/arcpy-dot-project-in-memory-featureclass/
+# create destination feature class using the source as a template to establish schema
+# and set destination spatial reference
+def copyFeaturesAndProject(source_fc, out_projected_fc, spatial_reference):
+  """ projects source_fc to out_projected_fc using cursors (and supports in_memory workspace) """
+  path, name = os.path.split(out_projected_fc)
+  arcpy.management.CreateFeatureclass(path, name,
+                                      arcpy.Describe(source_fc).shapeType,
+                                      template=source_fc,
+                                      spatial_reference=spatial_reference)
 
-def main():
+  # specify copy of all fields from source to destination
+  fields = ["Shape@"] + [f.name for f in arcpy.ListFields(source_fc) if not f.required]
+
+  # project source geometries on the fly while inserting to destination featureclass
+  with arcpy.da.SearchCursor(source_fc, fields, spatial_reference=spatial_reference) as source_curs, \
+       arcpy.da.InsertCursor(out_projected_fc, fields) as ins_curs:
+      for row in source_curs:
+        ins_curs.insertRow(row)
+
+def createViewshed(inputPoints, elevationRaster, Radius2_Input, \
+    Azimuth1_Input, Azimuth2_Input, OffsetA_Input, \
+    Radius1_Input, viewshed, sectorWedge, fullWedge):
+
+    #TODO: check inputs exist
 
     inputPointsCount = int(arcpy.GetCount_management(inputPoints).getOutput(0))
     if (inputPointsCount == 0) :
         raise Exception('No features in input feature set: ' + str(inputPoints))
 
     elevDesc = arcpy.Describe(elevationRaster)
-    Output_CS = elevDesc.spatialReference
+    elevationSR = elevDesc.spatialReference
 
-    if not Output_CS.type == "Projected":
-        msgErrorNonProjectedSurface = "Error: Input elevation raster must be in a projected coordinate system. Existing elevation raster is in {0}.".format(Output_CS.name)
+    if not elevationSR.type == "Projected":
+        msgErrorNonProjectedSurface = "Error: Input elevation raster must be in a projected coordinate system. Existing elevation raster is in {0}.".format(elevationSR.name)
         arcpy.AddError(msgErrorNonProjectedSurface)
         raise Exception(msgErrorNonProjectedSurface)
 
-    arcpy.env.outputCoordinateSystem = Output_CS
+    arcpy.env.outputCoordinateSystem = elevationSR
 
     donutWedges = []
     pieWedges = []
 
-    ####
-    ########End of Script Parameters##################
-    ####
-
     Point_Input = r"in_memory\tempPoints"
-    arcpy.CopyFeatures_management(inputPoints, Point_Input)
+    copyFeaturesAndProject(inputPoints, Point_Input, elevationSR)
 
-    #Check if point extent falls within surface extent
+    #Check if points falls within surface extent
     isWithin = surfaceContainsPoint(Point_Input, elevationRaster)
     if not isWithin:
         msgErrorPointNotInSurface = "Error: Input Observer(s) does not fall within the extent of the input surface: {0}!".format(os.path.basename(elevationRaster))
@@ -169,27 +188,27 @@ def main():
     if "OFFSETB" not in desc.Fields : 
         arcpy.AddField_management(Point_Input, "OFFSETB", "SHORT")
     # Set Target Height to 0
-    arcpy.CalculateField_management(Point_Input, "OFFSETB", "0")
+    arcpy.CalculateField_management(Point_Input, "OFFSETB", "0", "PYTHON_9.3", "")
 
     if "RADIUS2" not in desc.Fields : 
         arcpy.AddField_management(Point_Input, "RADIUS2", "SHORT")
-    arcpy.CalculateField_management(Point_Input, "RADIUS2", Radius2_Input)
+    arcpy.CalculateField_management(Point_Input, "RADIUS2", Radius2_Input, "PYTHON_9.3", "")
 
     if "AZIMUTH1" not in desc.Fields : 
         arcpy.AddField_management(Point_Input, "AZIMUTH1", "SHORT")
-    arcpy.CalculateField_management(Point_Input, "AZIMUTH1", Azimuth1_Input)
+    arcpy.CalculateField_management(Point_Input, "AZIMUTH1", Azimuth1_Input, "PYTHON_9.3", "")
 
     if "AZIMUTH2" not in desc.Fields : 
         arcpy.AddField_management(Point_Input, "AZIMUTH2", "SHORT")
-    arcpy.CalculateField_management(Point_Input, "AZIMUTH2", Azimuth2_Input)
+    arcpy.CalculateField_management(Point_Input, "AZIMUTH2", Azimuth2_Input, "PYTHON_9.3", "")
 
     if "OFFSETA" not in desc.Fields : 
         arcpy.AddField_management(Point_Input, "OFFSETA", "SHORT")
-    arcpy.CalculateField_management(Point_Input, "OFFSETA", OffsetA_Input)
+    arcpy.CalculateField_management(Point_Input, "OFFSETA", OffsetA_Input, "PYTHON_9.3", "")
 
     if "RADIUS1" not in desc.Fields : 
         arcpy.AddField_management(Point_Input, "RADIUS1", "SHORT")
-    arcpy.CalculateField_management(Point_Input, "RADIUS1", Radius1_Input)
+    arcpy.CalculateField_management(Point_Input, "RADIUS1", Radius1_Input, "PYTHON_9.3", "")
 
     arcpy.AddMessage("Buffering observers...")
     arcpy.Buffer_analysis(Point_Input, r"in_memory\OuterBuffer", "RADIUS2", "FULL", "ROUND", "NONE", "", "GEODESIC")
@@ -248,6 +267,25 @@ def main():
     arcpy.AddMessage("Finishing output features...")
     arcpy.Clip_analysis(r"in_memory\unclipped", sectorWedge, r"in_memory\dissolve")
     arcpy.Dissolve_management(r"in_memory\dissolve", viewshed, "gridcode", "", "MULTI_PART", "DISSOLVE_LINES")
+
+def main():
+
+    ########Script Parameters########
+
+    inputPoints = arcpy.GetParameterAsText(0)
+    elevationRaster = arcpy.GetParameterAsText(1)
+    Radius2_Input = arcpy.GetParameterAsText(2)
+    Azimuth1_Input = arcpy.GetParameterAsText(3)
+    Azimuth2_Input = arcpy.GetParameterAsText(4)
+    OffsetA_Input = arcpy.GetParameterAsText(5)
+    Radius1_Input = arcpy.GetParameterAsText(6)
+    viewshed = arcpy.GetParameterAsText(7)
+    sectorWedge = arcpy.GetParameterAsText(8)
+    fullWedge = arcpy.GetParameterAsText(9)
+
+    createViewshed(inputPoints, elevationRaster, \
+        Radius2_Input, Azimuth1_Input, Azimuth2_Input, OffsetA_Input, \
+        Radius1_Input, viewshed, sectorWedge, fullWedge)
 
 # MAIN =============================================
 if __name__ == "__main__":
